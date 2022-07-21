1ef8908f84f6e4b6e4d6c538d0fbf9a4d2a9c8a1
Vinícius Müller <vinigm.nho@gmail.com>
2022-07-21 19:34:28 -0300

    feat: first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Feline-Approved Ergokeyboards
+This repository contains pictures of different ergonomical keyboards being
+approved by felines.
+
+<p align="center">
+  <img src="./dactyl-manuform/dactyl-manuform-mini-approved.jpg">
+</p>
+
+# Contributing
+If you have a picture like this or want to take one now, feel free to open a
+pull request adding it to the repository!
+
+# Credits
+This project is inspired by https://www.zsa.io/moonlander/feline-approved/
---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Feline-Approved Ergokeyboards";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    { } // utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            python39Packages.grip
+          ];
+        };
+      });
+}
